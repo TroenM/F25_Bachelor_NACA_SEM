a9bb9dcd0d4b3ac984fb495bd42546538ed304cf
kiIIermort9 <145470255+kiIIermort9@users.noreply.github.com>
2025-02-14 12:51:06 +0100

    Some updates

--- Meshing/mesh_library.py
+++ Meshing/mesh_library.py
@@ -16,24 +16,23 @@ def mesh_gen_uniform_2D_grid(N_rows: int, N_cols: int,gridtype: str) -> meshio.M
     gridtype : str
         The gridtype is "triangle" if you want the grid to be made up of triangles, or "quad" if you want your grid to be made of squares
     '''
-    first_list = np.tile(np.arange(N_cols),N_rows)
-    second_list = np.repeat(np.arange(N_rows),N_cols)
+    first_list = np.tile(np.linspace(-2,3,N_cols),N_rows)
+    second_list = np.repeat(np.linspace(-2,2,N_rows),N_cols)
     points = np.array(((first_list,second_list,np.zeros(N_cols*N_rows)))).T
-    boundaries = np.where(
-            (points[:, 0] == np.min(points[:, 0])) |  # Left boundary
-            (points[:, 0] == np.max(points[:, 0])) |  # Right boundary
-            (points[:, 1] == np.min(points[:, 1])) |  # Bottom boundary
-            (points[:, 1] == np.max(points[:, 1]))    # Top boundary
-        )[0]
+    boundary_edges = np.array([
+            np.where((points[:, 1] == np.min(points[:, 1])))[0],   # Left boundary
+            np.where((points[:, 1] == np.max(points[:, 1])))[0],   # Right boundary
+            np.where((points[:, 0] == np.min(points[:, 0])))[0],   # Bottom boundary
+            np.where((points[:, 0] == np.max(points[:, 0])))[0]    # Top boundary
+    ])
+    boundary_markers = {"bottom": [0], "right": [1], "top": [2], "left": [3]} 
     if gridtype.lower() == "quad":
         squares = np.zeros(((N_cols-1)*(N_rows-1),4), dtype=int)
         for i in range(N_cols-1):
             for j in range(N_rows-1):
                 squares[(N_cols-1)*j+i,:] = [i+j*N_cols, i+1+j*N_cols, i+1+(j+1)*N_cols, i+(j+1)*N_cols]
         cells = [(gridtype, squares)]
-        point_data = {"boundary": np.zeros(len(points), dtype=int)}
-        point_data["boundary"][boundaries] = 1  # Mark boundary nodes as 1
-        return meshio.Mesh(points=points, cells=cells, point_data=point_data)
+        return meshio.Mesh(points=points, cells=cells, cell_sets={"boundary": boundary_edges})
     
     elif gridtype.lower() == "triangle":
         triangles = np.zeros(((N_cols-1)*(N_rows-1)*2,3), dtype=int)
@@ -42,9 +41,7 @@ def mesh_gen_uniform_2D_grid(N_rows: int, N_cols: int,gridtype: str) -> meshio.M
                 triangles[((N_cols-1)*j+i)*2,:] = [i+j*N_cols, i+1+j*N_cols, i+1+(j+1)*N_cols]
                 triangles[((N_cols-1)*j+i)*2+1,:] = [i+j*N_cols, i+1+(j+1)*N_cols, i+(j+1)*N_cols]
         cells = [(gridtype, triangles)]
-        point_data = {"boundary": np.zeros(len(points), dtype=int)}
-        point_data["boundary"][boundaries] = 1  # Mark boundary nodes as 1
-        return meshio.Mesh(points=points, cells=cells, point_data=point_data)
+        return meshio.Mesh(points=points, cells=cells, cell_sets={"boundary": boundary_edges})
     else:
         raise ValueError("That is not a valid gridtype, it should either be triangle or quad")
 
