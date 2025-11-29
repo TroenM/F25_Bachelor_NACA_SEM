56a6bc986c0dfac112e7b61f1f443e235d4dd2b8
Troen <mtroen89@gmail.com>
2025-11-29 11:32:34 +0100

    HPC submission script

---
+++ Functional/submit_HPC.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#BSUB -J NACA_SEM_HPC
+#BSUB -o NACA_SEM_HPC.out
+#BSUB -e NACA_SEM_HPC.err
+
+#BSUB -q hpc
+#BSUB -R "rusage[mem=4GB]"
+#BSUB -N
+#BSUB -W 12:00 
+
+#BSUB -n 3
+#BSUB -R "span[hosts=1]"
+
+
+module purge
+source ~/venv-firedrake/bin/activate
+
+python3 -u ClassFSSolver.py
